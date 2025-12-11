0d50ef4cf1630dcc11d7bdd1bc3d56f1123f48c5
Vineeth2002 <83575425+Vineeth2002@users.noreply.github.com>
2025-12-11 21:59:34 +0530

    Create __init__.py

---
+++ collectors/google_trends/__init__.py
@@ -0,0 +1,7 @@
+"""
+DriftMonitor Google Trends collector package.
+
+Lightweight collector that uses pytrends when available and falls back to a bundled sample.
+"""
+
+__all__ = ["GoogleTrendsCollector"]
