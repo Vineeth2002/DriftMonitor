a3f0e47d0db3be00f049728cfaa4ca0187c051c0
Vineeth2002 <83575425+Vineeth2002@users.noreply.github.com>
2025-12-14 01:40:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="driftmonitor",
     version="1.0.0",
+    description="AI Safety & Risk Drift Monitoring Platform",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
